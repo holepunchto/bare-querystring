c9bee874cde48249bf12bf073bb293750b847b41
Yasser Nascimento <dev@yasser.email>
2024-12-13 17:22:17 -0300

    Switch to Prettier

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const escape = exports.escape = function escape (str) {
+const escape = (exports.escape = function escape(str) {
   return encodeURIComponent(str)
-}
+})
 
-const unescape = exports.unescape = function unescape (str) {
+const unescape = (exports.unescape = function unescape(str) {
   return decodeURIComponent(str)
-}
+})
 
-exports.parse = function parse (str, sep = '&', eq = '=') {
+exports.parse = function parse(str, sep = '&', eq = '=') {
   const obj = Object.create(null)
 
   for (const tmp of str.split(sep)) {
@@ -17,17 +17,14 @@ exports.parse = function parse (str, sep = '&', eq = '=') {
     key = unescape(key)
     value = unescape(value || '')
 
-    obj[key] = key in obj
-      ? [].concat(obj[key], value)
-      : value
+    obj[key] = key in obj ? [].concat(obj[key], value) : value
   }
 
   return obj
 }
 
-exports.stringify = function stringify (obj, sep = '&', eq = '=') {
-  return Object
-    .entries(obj)
+exports.stringify = function stringify(obj, sep = '&', eq = '=') {
+  return Object.entries(obj)
     .map(([key, value]) =>
       (Array.isArray(value) ? value : [value])
         .map((value) => escape(key) + eq + escape(value))
