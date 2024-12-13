5bbc877e5daac0b842872281694c303cfef48141
Yasser Nascimento <dev@yasser.email>
2024-12-13 17:35:25 -0300

    Add TypeScript typings

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export function parse(str: string, sep?: string, eq?: string): {}
+export function decode(str: string, sep?: string, eq?: string): {}
+
+export function stringify(obj: {}, sep?: string, eq?: string): string
+export function encode(obj: {}, sep?: string, eq?: string): string
