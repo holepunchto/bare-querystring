54cf37f41c9e6712b7218c4040838fc2e523d502
Yasser Nascimento <dev@yasser.email>
2024-12-16 10:49:04 -0300

    Stricter type for the `decode` return

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,15 @@
-export function parse(str: string, sep?: string, eq?: string): {}
-export function decode(str: string, sep?: string, eq?: string): {}
+export function parse(
+  str: string,
+  sep?: string,
+  eq?: string
+): Record<string, string | string[]>
+
+export function decode(
+  str: string,
+  sep?: string,
+  eq?: string
+): Record<string, string | string[]>
 
 export function stringify(obj: {}, sep?: string, eq?: string): string
+
 export function encode(obj: {}, sep?: string, eq?: string): string
