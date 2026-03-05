4bdbb5eaaff319262d001ac4f1c6d79b5871a4fe
Yasser Nascimento <dev@yasser.email>
2026-03-05 18:45:46 -0300

    Switch to `prettier-config-holepunch`

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,6 @@
-export function parse(
-  str: string,
-  sep?: string,
-  eq?: string
-): Record<string, string | string[]>
+export function parse(str: string, sep?: string, eq?: string): Record<string, string | string[]>
 
-export function decode(
-  str: string,
-  sep?: string,
-  eq?: string
-): Record<string, string | string[]>
+export function decode(str: string, sep?: string, eq?: string): Record<string, string | string[]>
 
 export function stringify(obj: {}, sep?: string, eq?: string): string
 
